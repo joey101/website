321a6bdf0ae8c4b5636f8d9926d3ca9ef4a69ace
joey101 <jawad.tanana@gmail.com>
2023-01-20 21:51:16 +1100

    stuff

--- Moe/moe.js
+++ Moe/moe.js
@@ -2,7 +2,7 @@ let loadingBar = document.getElementById("loading-bar");
   var loading = 0;
   var loadingSpeed = 0.4;
   
-  function move() {
+  function updateLoading() {
     loading += loadingSpeed;
     if (loading > 50) {
       loadingSpeed *= 0.99;
@@ -10,7 +10,7 @@ let loadingBar = document.getElementById("loading-bar");
     loadingBar.style.width = loading + "%";
     // End at 99.5% filled.
     if (loading < 99.5) {
-      setTimeout(move, 10);
+      setTimeout(updateLoading, 10);
     }
   }
-  move();
+  updateLoading();
